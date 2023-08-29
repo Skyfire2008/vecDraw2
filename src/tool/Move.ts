8b6f5cb5d7fd2069655b3b8879b8e90281161956
Leonid Darovskikh <skyfire2008@gmail.com>
2023-08-29 08:01:51 +0200

    move WIP

---
+++ src/tool/Move.ts
@@ -0,0 +1,20 @@
+class Move implements Tool {
+
+	readonly name = "Move";
+
+	constructor() { }
+
+	public onMouseDown(e: MyMouseEvent, ctx: AppContextProps) {
+	}
+
+	public onMouseMove(e: MyMouseEvent, ctx: AppContextProps) {
+	}
+
+	public onMouseUp(e: MyMouseEvent, ctx: AppContextProps) {
+	}
+
+	public onEnable(ctx: AppContextProps) {
+	}
+
+	public onDisable(ctx: AppContextProps) { }
+}
